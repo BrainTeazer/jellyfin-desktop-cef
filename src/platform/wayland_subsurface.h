a510507aee161e514df3768b31bb168ba4e24921
BrainTeazer <brainteazer123@gmail.com>
2026-03-22 09:52:29 +0100

    lint wayland_subsurface.h

--- src/platform/wayland_subsurface.h
+++ src/platform/wayland_subsurface.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #define VK_USE_PLATFORM_WAYLAND_KHR
-#include <vulkan/vulkan.h>
-#include <wayland-client.h>
+#include "video_surface.h"
 #include "wayland-protocols/color-management-v1-client.h"
 #include "wayland-protocols/viewporter-client.h"
-#include "video_surface.h"
-#include <mpv/render_vk.h>
 #include <atomic>
+#include <mpv/render_vk.h>
 #include <vector>
+#include <vulkan/vulkan.h>
+#include <wayland-client.h>
 
 struct SDL_Window;
 
@@ -22,9 +22,13 @@ public:
     WaylandSubsurface();
     ~WaylandSubsurface() override;
 
-    bool init(SDL_Window* window, VkInstance instance, VkPhysicalDevice physicalDevice,
-              VkDevice device, uint32_t queueFamily,
-              const char* const* extensions, int numExtensions,
+    bool init(SDL_Window*                      window,
+              VkInstance                       instance,
+              VkPhysicalDevice                 physicalDevice,
+              VkDevice                         device,
+              uint32_t                         queueFamily,
+              const char* const*               extensions,
+              int                              numExtensions,
               const VkPhysicalDeviceFeatures2* features) override;
     bool createSwapchain(int width, int height) override;
     bool recreateSwapchain(int width, int height) override;
@@ -37,34 +41,33 @@ public:
     // Accessors
     wl_display* display() const { return wl_display_; }
     wl_surface* surface() const { return mpv_surface_; }
-    VkFormat swapchainFormat() const override { return VK_FORMAT_UNDEFINED; }
-    VkExtent2D swapchainExtent() const override { return swapchain_extent_; }
-    bool isHdr() const override { return true; }
-    uint32_t width() const override { return swapchain_extent_.width; }
-    uint32_t height() const override { return swapchain_extent_.height; }
+    VkFormat    swapchainFormat() const override { return VK_FORMAT_UNDEFINED; }
+    VkExtent2D  swapchainExtent() const override { return swapchain_extent_; }
+    bool        isHdr() const override { return true; }
+    uint32_t    width() const override { return swapchain_extent_.width; }
+    uint32_t    height() const override { return swapchain_extent_.height; }
 
     // Vulkan handles for mpv
-    VkInstance vkInstance() const override { return instance_; }
-    VkPhysicalDevice vkPhysicalDevice() const override { return physical_device_; }
-    VkDevice vkDevice() const override { return device_; }
-    VkQueue vkQueue() const override;
-    uint32_t vkQueueFamily() const override;
-    PFN_vkGetInstanceProcAddr vkGetProcAddr() const override { return vkGetInstanceProcAddr; }
+    VkInstance                       vkInstance() const override { return instance_; }
+    VkPhysicalDevice                 vkPhysicalDevice() const override { return physical_device_; }
+    VkDevice                         vkDevice() const override { return device_; }
+    VkQueue                          vkQueue() const override;
+    uint32_t                         vkQueueFamily() const override;
+    PFN_vkGetInstanceProcAddr        vkGetProcAddr() const override { return vkGetInstanceProcAddr; }
     const VkPhysicalDeviceFeatures2* features() const override { return &features2_; }
-    const char* const* deviceExtensions() const override;
-    int deviceExtensionCount() const override;
+    const char* const*               deviceExtensions() const override;
+    int                              deviceExtensionCount() const override;
 
-    VkSurfaceKHR vkSurface() const { return vk_surface_; }
+    VkSurfaceKHR               vkSurface() const { return vk_surface_; }
     const mpv_display_profile& displayProfile() const { return display_profile_; }
 
     void commit();
     void hide() override;
-    void setColorspace() override {}  // Mesa handles via swapchain
+    void setColorspace() override {} // Mesa handles via swapchain
     void setDestinationSize(int width, int height) override;
     void initDestinationSize(int width, int height);
 
-    static void registryGlobal(void* data, wl_registry* registry,
-                               uint32_t name, const char* interface, uint32_t version);
+    static void registryGlobal(void* data, wl_registry* registry, uint32_t name, const char* interface, uint32_t version);
     static void registryGlobalRemove(void* data, wl_registry* registry, uint32_t name);
 
 private:
@@ -72,34 +75,34 @@ private:
     bool createSubsurface(wl_surface* parentSurface);
     void queryDisplayProfile();
 
-    wl_display* wl_display_ = nullptr;
-    wl_compositor* wl_compositor_ = nullptr;
+    wl_display*       wl_display_       = nullptr;
+    wl_compositor*    wl_compositor_    = nullptr;
     wl_subcompositor* wl_subcompositor_ = nullptr;
-    wl_surface* mpv_surface_ = nullptr;
-    wl_subsurface* mpv_subsurface_ = nullptr;
+    wl_surface*       mpv_surface_      = nullptr;
+    wl_subsurface*    mpv_subsurface_   = nullptr;
 
     wp_viewporter* viewporter_ = nullptr;
-    wp_viewport* viewport_ = nullptr;
+    wp_viewport*   viewport_   = nullptr;
 
-    VkInstance instance_ = VK_NULL_HANDLE;
+    VkInstance       instance_        = VK_NULL_HANDLE;
     VkPhysicalDevice physical_device_ = VK_NULL_HANDLE;
-    VkDevice device_ = VK_NULL_HANDLE;
-    VkQueue queue_ = VK_NULL_HANDLE;
-    uint32_t queue_family_ = 0;
-    VkSurfaceKHR vk_surface_ = VK_NULL_HANDLE;
+    VkDevice         device_          = VK_NULL_HANDLE;
+    VkQueue          queue_           = VK_NULL_HANDLE;
+    uint32_t         queue_family_    = 0;
+    VkSurfaceKHR     vk_surface_      = VK_NULL_HANDLE;
 
     VkPhysicalDeviceVulkan11Features vk11_features_{};
     VkPhysicalDeviceVulkan12Features vk12_features_{};
-    VkPhysicalDeviceFeatures2 features2_{};
-    std::vector<const char*> enabled_extensions_;
+    VkPhysicalDeviceFeatures2        features2_{};
+    std::vector<const char*>         enabled_extensions_;
 
-    wp_color_manager_v1* color_manager_ = nullptr;
-    wl_output* wl_output_ = nullptr;
-    mpv_display_profile display_profile_ = {};
+    wp_color_manager_v1* color_manager_   = nullptr;
+    wl_output*           wl_output_       = nullptr;
+    mpv_display_profile  display_profile_ = {};
 
     VkExtent2D swapchain_extent_ = {0, 0};
 
-    std::atomic<int> pending_dest_width_{0};
-    std::atomic<int> pending_dest_height_{0};
+    std::atomic<int>  pending_dest_width_{0};
+    std::atomic<int>  pending_dest_height_{0};
     std::atomic<bool> dest_pending_{false};
 };
